a6ec4ddf343b5e4f9db5dd97d841b1477d387502
Georgi Valkov <georgi.t.valkov@gmail.com>
2018-04-28 17:20:39 +0200

    Update tasks.py to new invoke API

--- tasks.py
+++ tasks.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8; -*-
-
 import os
-from invoke import run, task
+
+from invoke import task
 
 from tests.refapi import JenkinsCLI
 from tests.install import JenkinsInstall
@@ -9,17 +8,14 @@ from tests.install import JenkinsInstall
 
 JENKINS_WAR_URL = 'http://mirrors.jenkins-ci.org/war/latest/jenkins.war'
 JENKINS_CLI_JAR = 'tests/tmp/latest/jenkins-cli.jar'
-JENKINS_HOST    = 'localhost'
-JENKINS_PORT    = 60888
-JENKINS_CPORT   = 60887
+JENKINS_HOST = 'localhost'
+JENKINS_PORT = 60888
+JENKINS_CPORT = 60887
+JENKINS_DESTDIR = os.path.join(os.environ.get('TMPDIR', '/tmp'), 'jenkins-webapi-tests/jenkins-latest')
 
-# Directory in which to download and run jenkins.war.
-JENKINS_DESTDIR = os.path.join(
-    os.environ.get('TMPDIR', '/tmp'),
-    'jenkins-webapi-tests/jenkins-latest')
 
 @task(name='start-jenkins')
-def start_jenkins():
+def start_jenkins(ctx):
     config = {
         'url':     JENKINS_WAR_URL,
         'destdir': JENKINS_DESTDIR,
@@ -30,29 +26,29 @@ def start_jenkins():
 
     ji = JenkinsInstall(**config)
     ji.bootstrap()
-
-    print()
     ji.start()
-
-    print()
     ji.wait()
 
+
 @task(name='stop-jenkins')
-def stop_jenkins():
-    run('echo 0 | nc %s %s' % (JENKINS_HOST, JENKINS_CPORT))
+def stop_jenkins(ctx):
+    ctx.run('echo 0 | nc %s %s' % (JENKINS_HOST, JENKINS_CPORT))
+
 
 @task(name='remove-jobs')
-def remove_jobs():
+def remove_jobs(ctx):
     url = 'http://%s:%s' % (JENKINS_HOST, JENKINS_PORT)
     cli = JenkinsCLI(url, JENKINS_CLI_JAR)
 
     for job in cli.jobs():
         cli.delete_job(job)
 
-@task
-def test():
-	run('py.test tests -xvs')
 
 @task
-def coverage():
-	run('py.test --cov-report term-missing --cov jenkins tests')
+def test(ctx):
+    ctx.run('py.test tests -xvs')
+
+
+@task
+def coverage(ctx):
+    ctx.run('py.test --cov-report term-missing --cov jenkins tests')
